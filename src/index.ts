fad378d8ced672ec60239fb1723cd804faf1c610
Gary Storey <garystorey655@gmail.com>
2023-02-17 12:18:08 -0500

    update readme formatting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Game } from "./game"
 import { addMummys, btwn } from "./utils"
 
-
 const animate = () => {
   if (game.gameloop.sprites.length === 1) {
     game.stop()
@@ -9,6 +8,7 @@ const animate = () => {
   }
   const chance = btwn(1, 100)
   const val = btwn(1, 50)
+  // this is not very efficient but thats not the point
   if (chance > 90) game.remove(`m${val}`)
 }
 
@@ -17,7 +17,7 @@ const game = new Game({
   gravity: { x: 0, y: 0 },
   parent: document.body,
   animate,
-  sprites: addMummys(50),
+  sprites: addMummys(5),
 })
 
 game.start()
--- src/utils.ts
+++ src/utils.ts
@@ -1,34 +1,35 @@
-
-
-export function btwn(pMin = 1, pMax = 1_000_000_000){
-    pMin = Math.round(pMin);
-    pMax = Math.round(pMax);
-    if (pMax < pMin) { let t = pMin; pMin = pMax; pMax = t;}
-    return Math.floor(Math.random() * (pMax+1 - pMin) + pMin);
+export function btwn(pMin = 1, pMax = 1_000_000_000) {
+  pMin = Math.round(pMin)
+  pMax = Math.round(pMax)
+  if (pMax < pMin) {
+    let t = pMin
+    pMin = pMax
+    pMax = t
+  }
+  return Math.floor(Math.random() * (pMax + 1 - pMin) + pMin)
 }
 
-export function addMummys(num:number){
+export function addMummys(num: number) {
   const a = []
-  for(let i=0;i<num;i++){
+  for (let i = 0; i < num; i++) {
     a.push(createMummy(i))
   }
   return a
 }
 
-export function createMummy(i:number):SpriteInfo {
-    const x = btwn(50,250)
-    return {
-      coords: {x:btwn(15,150),y:btwn(10,450)},
-      scale:{ x:.25,y:.25},
-      loop:true,
-      id:`m${i}`,
-      animationSpeed: 1,
-      image: {
-        src: "mummy.png",
-        frames:32,
-      },
-      size: {x:50,y:50},
-      movement: {x:x/100,y:0}
-    }
+export function createMummy(i: number): SpriteInfo {
+  const x = btwn(50, 250)
+  return {
+    coords: { x: btwn(15, 150), y: btwn(10, 450) },
+    scale: { x: 0.25, y: 0.25 },
+    loop: true,
+    id: `m${i}`,
+    animationSpeed: 5,
+    image: {
+      src: "mummy.png",
+      frames: 32,
+    },
+    size: { x: 50, y: 50 },
+    movement: { x: x / 100, y: 0 },
   }
-  
+}
